bb94d175f85f907d2fe87969e15cbe95a04b7fb2
Valeria-Montisci <valeria_montisci@hotmail.com>
2022-03-14 06:42:05 +1100

    set the search engine

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 
 import './App.css';
-
+import Dictionary from "./Dictionary.js"
 function App() {
   return (
     <div className="App">
-     <p>Dictionary App</p>
-     
+   <div className='container'>
+<main>
+  <Dictionary />
+</main>
+<footer> Coded By Valeria Montisci</footer>
+   </div>
     </div>
   );
 }
