b003346d129697eb12a3c6b615a5e9fa169ed18b
Valeria-Montisci <valeria_montisci@hotmail.com>
2022-03-22 06:56:25 +1100

    styled app

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
 
 import './App.css';
 import Dictionary from "./Dictionary.js"
-function App() {
+export default function App() {
   return (
     <div className="App">
-   <div className='container'>
-<main>
-  <Dictionary />
-</main>
-<footer> Coded By Valeria Montisci</footer>
-   </div>
+      <div className="container">
+        <header className="App-header">
+        
+        </header>
+        <main>
+          <Dictionary defaultKeyword="cat" />
+        </main>
+        <footer className="App-footer">
+          <small>Coded by SheCodes</small>
+        </footer>
+      </div>
     </div>
   );
 }
-
-export default App;
